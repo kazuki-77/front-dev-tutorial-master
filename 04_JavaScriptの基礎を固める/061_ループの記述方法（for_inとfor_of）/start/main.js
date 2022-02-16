2a2c4cc8051937e34f926f44fb96537780b504f5
kauzki-77 <k.sora.7861@gmail.com>
2022-02-17 00:21:22 +0900

    [add] 配列のループ処理

--- 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
+++ 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
@@ -1,5 +1,12 @@
 const arry = [1, 2, 3, 4, 5, 6];
 
-for (let i = 0; i < arry.length; i++) {
-    console.log(i);
+for (let v of arry) {
+    console.log(v);
 }
+
+// for~in は配列をループさせた場合は配列の番号(添字)が出力される
+// console.log()の引数を(arry[i])とすることで配列の中の値が出力される
+// console.log()の引数を(i, arry[i])とすることで両方を出力することができる
+
+// for~of は配列をループさせた場合は配列の値が出力される
+// of を使う場合は添字は使わないようにする
