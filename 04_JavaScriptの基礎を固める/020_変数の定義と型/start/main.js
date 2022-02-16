610e0cfb2eeabdefaaea021d638f2cd3591bb868
kauzki-77 <k.sora.7861@gmail.com>
2022-02-16 21:53:36 +0900

    [add] js 基礎

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,19 @@
+// var, let, const
+
+let name = 'Tom';
+name = "tim";
+console.log('hello' + name);
+
+// Number, string, Boolean, Undefined, Null, Symbol
+
+// 動的型付け言語
+let variable = 'str'; // string型
+variable = 12; // Number型
+variable = false; // Boolean型
+variable = undefined; // Undefined型
+
+console.log(typeof variable);
+
+// 静的型付け言語
+// 宣言する時に型を決める
+int num = 12;
