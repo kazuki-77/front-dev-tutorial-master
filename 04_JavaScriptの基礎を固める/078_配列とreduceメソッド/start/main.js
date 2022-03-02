b20053fff234fab9c6693e6bc1ab2eeca18d68fa
kauzki-77 <k.sora.7861@gmail.com>
2022-03-02 23:03:46 +0900

    [add]reduceメソッド

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,20 @@
 const arry = [1,2,3,4,5];
+// reduceメソッド・・・配列の中身を足し合わせたり、文字列として結合するときに使う
+// accu・・・accumulationの略。蓄積という意味。
+arry.reduce(
+  function(accu, curr){
+    console.log(accu, curr);
+    return accu + curr; // 前のループの足し算
+}, 10);
+
+const str = 'animation';
+const strArry = str.split('');
+
+console.log(strArry);
+
+const result = strArry.reduce((accu, curr) => {
+  
+  return `${accu}<${curr}>`;
+}, "");
+
+console.log(result)
